1b73ab1e8f3d7146b23251289acd6c1c4360f408
shohey1226 <shoheik@cpan.org>
2022-04-14 02:54:33 +0000

    added build script

---
+++ scripts/build_ruby.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+yum install -y git bzip2 openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel gcc gcc-c++ autoconf automake libtool bison
+
+git clone https://github.com/rbenv/ruby-build.git                                                                                                                                                    
+PREFIX=/usr/local ./ruby-build/install.sh
+
+RUBY_VERSION=$1
+ruby-build $RUBY_VERSION . 
+
